a27d6eb0e3a3ceef14cbbfbc6a00d2621afb5ae2
inwoo <dldlsdn4@naver.com>
2022-10-28 01:38:26 +0900

    20221028 study

---
+++ lib/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NakaoLogin extends StatefulWidget {
+  const NakaoLogin({super.key});
+
+  @override
+  State<NakaoLogin> createState() => _NakaoLoginState();
+}
+
+class _NakaoLoginState extends State<NakaoLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text("Nukao login page"),
+      ),
+    );
+  }
+}
